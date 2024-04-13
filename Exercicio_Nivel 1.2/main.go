07d003159f92941dad54e4c71d2db78f14e58fca
Danilo Cassiano <93688833+danilocassiano@users.noreply.github.com>
2024-04-12 23:18:14 -0300

    feat(Exercicio): Exercicio 1.2

---
+++ Exercicio_Nivel 1.2/main.go
@@ -0,0 +1,27 @@
+// Utilizando a solução do exercício anterior:
+//     1. Em package-level scope, atribua os seguintes valores às variáveis:
+//         1. para "x" atribua 42
+//         2. para "y" atribua "James Bond"
+//         3. para "z" atribua true
+//     2. Na função main:
+//         1. Use fmt.Sprintf para atribuir todos esses valores a uma única variável. Faça essa atribuição de tipo string a uma variável de nome "s" utilizando o operador curto de declaração.
+//         2. Demonstre a variável "s".
+
+package main
+
+import "fmt"
+
+var x int
+var y string
+var z bool
+
+func main(){	
+	x = 42
+	y = "James Bond"
+	z = true
+ 
+	s := fmt.Sprintf("%v\t %v\t %v\t", x, y, z)
+	
+	fmt.Println(s)
+
+}
